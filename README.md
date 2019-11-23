09a946f8432190aaa3bdb164912ff8a0e796ba32
saumya <2saumyaray@gmail.com>
2019-11-23 12:16:14 +0530

    init

---
+++ README.md
@@ -0,0 +1,19 @@
+Remote MySQL
+========================
+
+This uses [mysql][1] package from NPM. The [docs][2] are here too.
+
+```
+npm install mysql --save
+```
+
+
+
+
+
+
+
+
+
+[1]: https://www.npmjs.com/package/mysql
+[2]: https://github.com/mysqljs/mysql#table-of-contents
---
+++ index.js
@@ -0,0 +1,43 @@
+console.log('Remote SQL');
+
+var config = require('./remotemysql.config');
+var mysql = require('mysql');
+
+
+//console.log(config);
+
+console.log('+--------------- START ---------------------');
+
+//var mysql = require('mysql');
+var connection = mysql.createConnection({
+    host: config.host,
+    port: config.port,
+    user: config.user,
+    password: config.password,
+    database: config.database
+});
+//
+connection.connect( function(error){
+    if (error) {
+        console.error('error connecting: ' + error.stack);
+        return;
+    }
+    console.log('connected as id ' + connection.threadId);
+} );
+//TODO
+/*
+connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+    if (error) throw error;
+    console.log('The solution is: ', results[0].solution);
+  });
+*/
+/*
+connection.query('SELECCT * from *', function(error,results,fields){
+    if(error) throw error;
+    console.log(results);
+});
+*/
+//
+connection.end();
+
+console.log('+---------------- END ----------------------');
